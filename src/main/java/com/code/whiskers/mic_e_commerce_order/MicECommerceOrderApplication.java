2dbcde3417262158482a37f2e864397a1446e443
Carlos <gohercarlos@gmail.com>
2024-05-27 19:04:43 -0500

    Inicio del microservicio de ordenes

---
+++ src/main/java/com/code/whiskers/mic_e_commerce_order/MicECommerceOrderApplication.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicECommerceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicECommerceOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/whiskers/mic_e_commerce_order/MicECommerceOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicECommerceOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
