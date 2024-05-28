98e27f8e4626a0fae2db5cb951204b03b444a15c
Carlos <gohercarlos@gmail.com>
2024-05-27 20:50:08 -0500

    ADD: Creacion de los modelo y caso de uso de los item de los item de la orden

---
+++ src/main/java/com/code/whiskers/mic_e_commerce_order/order_item/application/port/in/OrderItemPortIn.java
@@ -0,0 +1,14 @@
+package com.code.whiskers.mic_e_commerce_order.order_item.application.port.in;
+
+import com.code.whiskers.mic_e_commerce_order.common.domain.ResultResponse;
+import com.code.whiskers.mic_e_commerce_order.order_item.domain.entities.OrderItem;
+
+public interface OrderItemPortIn {
+
+    public ResultResponse<OrderItem> consult(int page, int size, String search, String filter);
+    public OrderItem show(Long id);
+    public OrderItem create(OrderItem orderItem);
+    public OrderItem update(OrderItem orderItem, Long id);
+    public OrderItem delete(Long id);
+
+}
---
+++ src/main/java/com/code/whiskers/mic_e_commerce_order/order_item/application/services/OrderItemServices.java
@@ -0,0 +1,40 @@
+package com.code.whiskers.mic_e_commerce_order.order_item.application.services;
+
+import com.code.whiskers.mic_e_commerce_order.common.domain.ResultResponse;
+import com.code.whiskers.mic_e_commerce_order.order_item.application.port.in.OrderItemPortIn;
+import com.code.whiskers.mic_e_commerce_order.order_item.application.port.out.OrderItemPortOut;
+import com.code.whiskers.mic_e_commerce_order.order_item.domain.entities.OrderItem;
+
+public class OrderItemServices implements OrderItemPortIn {
+
+    private final OrderItemPortOut orderItemPortOut;
+
+    public OrderItemServices(OrderItemPortOut orderItemPortOut) {
+        this.orderItemPortOut = orderItemPortOut;
+    }
+
+    @Override
+    public ResultResponse<OrderItem> consult(int page, int size, String search, String filter) {
+        return this.orderItemPortOut.consult(page,size,search,filter);
+    }
+
+    @Override
+    public OrderItem show(Long id) {
+        return this.orderItemPortOut.show(id);
+    }
+
+    @Override
+    public OrderItem create(OrderItem orderItem) {
+        return this.orderItemPortOut.create(orderItem);
+    }
+
+    @Override
+    public OrderItem update(OrderItem orderItem, Long id) {
+        return orderItemPortOut.update(orderItem, id);
+    }
+
+    @Override
+    public OrderItem delete(Long id) {
+        return orderItemPortOut.delete(id);
+    }
+}
